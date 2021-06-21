791cb5e461a6996dbc21ae563a51247c3d2926eb
lilja-johan <jplilja93@gmail.com>
2021-06-21 15:27:50 +0200

    Character saerch method completed!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      boolean hasLetter = charSearch("Welcome to the ultimate Git-course!", 'e');
+
+        System.out.println(hasLetter);
+    }
+
+    public static boolean charSearch(String s, char x) {
+        for (char c : s.toCharArray()) {
+            if (c == x) return true;
+        }
+        return false;
     }
 }
