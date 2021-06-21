bc92a4c7dcaad632984ac4869af1d390e43e9fdb
Per H <per_hakansson@live.se>
2021-06-21 15:19:14 +0200

    New branch touppercase

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+    /*
+    public static String toUpperCase (String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            sb.append(Character.toUpperCase(ch));
+        }
+        return sb.toString();
+    }
+
+     */
 }
