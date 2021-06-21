8b08446fda5e5621e20887f6ed4a3f3f24391e5f
Jenny Larruy <jenny.larruy@gmail.com>
2021-06-21 15:16:36 +0200

    adding method trimAString

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static String trimAString(String s){
+        return s.trim();
+    }
+
 }
